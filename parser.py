fbf9fa102025573c032a98b6c9e2d92954e8b0b0
ding-ding <artyom.endin@gmail.com>
2014-03-28 17:39:44 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
+import time
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
@@ -33,16 +34,44 @@ def parse_page(url_page):
     html_doc = urlopen(url_page).read()
     soup = BeautifulSoup(html_doc)
 
-    adverts = soup.find_all("div", class_="b-snippet ungrouped unmarked editable")
+    adverts = soup.find_all("div", class_="title")
     for advert in adverts:
-        db.am_ru.save({"advert": advert})
+        advert_url = advert.a.get("href")
+        print advert.a.get("href")
+        db.am_ru.insert({"advert_url": advert_url})
 
-pages = get_count_pages(url)
-print pages
 
-#advert = urlopen('').read()
-#advert_soup = BeautifulSoup(advert)
-# for i in xrange(0, int(pages)):
-#     print url_page + str(i)
+def parse_pages(count_pages):
+    for i in xrange(0, int(count_pages)):
+        print i
+        page = url_page + str(i)
+        print page
+        parse_page(page)
 
-parse_page(url_page+'1')
+
+def parse_advert(url_advert):
+    for advert in adverts:
+        print advert["advert_url"]
+
+# db.pages.remove()
+# db.am_ru.remove()
+# pages = get_count_pages(url)
+# print pages
+# parse_pages(pages)
+
+for advert in db.am_ru.find():
+    print advert.get("advert_url")
+    advert_url = str(advert.get("advert_url"))
+    print advert_url
+    html_doc = urlopen(advert_url).read()
+    soup = BeautifulSoup(html_doc)
+
+    images = soup.find("li", class_="b-rama-thumbs__item")
+    for image in images:
+        image_soup = BeautifulSoup(image)
+        src = image_soup.li
+        print src
+    #print type(images)
+    #db.am_ru_adverts.insert({"advert_html": advert_html})
+
+    time.sleep(300)
