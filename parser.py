b5f9f35bb9fdd3e77c2f174ed842ad28312f136c
Artyom Endin <artyom.endin@gmail.com>
2014-03-30 22:26:36 +0400

    download image

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ import pymongo
 import time
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+from PIL import Image
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.parser
@@ -59,19 +60,45 @@ def parse_advert(url_advert):
 # print pages
 # parse_pages(pages)
 
-for advert in db.am_ru.find():
-    print advert.get("advert_url")
-    advert_url = str(advert.get("advert_url"))
-    print advert_url
-    html_doc = urlopen(advert_url).read()
-    soup = BeautifulSoup(html_doc)
+# for advert in db.am_ru.find():
+#     print advert.get("advert_url")
+#     advert_url = str(advert.get("advert_url"))
+#     print advert_url
+#     html_doc = urlopen(advert_url).read()
+#     soup = BeautifulSoup(html_doc)
+#
+#     images = soup.find("li", class_="b-rama-thumbs__item")
+#     for image in images:
+#         image_soup = BeautifulSoup(image)
+#         src = image_soup.li
+#         print src
+#     #print type(images)
+#     #db.am_ru_adverts.insert({"advert_html": advert_html})
+#
+#     time.sleep(300)
 
-    images = soup.find("li", class_="b-rama-thumbs__item")
-    for image in images:
-        image_soup = BeautifulSoup(image)
-        src = image_soup.li
-        print src
-    #print type(images)
-    #db.am_ru_adverts.insert({"advert_html": advert_html})
+html_doc = urlopen("http://nnov.am.ru/used/hyundai/ix35/avs-avtoregion-nn--9fa3edb9/#snp3").read()
+soup = BeautifulSoup(html_doc)
 
-    time.sleep(300)
+images = []
+ul = soup.find_all("li", class_="b-rama-thumbs__item")
+for li in ul:
+    images.append(li.a["data-original"])
+
+ii = 0
+for image in images:
+    fileimage = urlopen(image).read()
+    f = open(str(ii)+".jpg", "wb")
+    f.write(fileimage)
+    f.close
+    print image
+    img = Image.open(str(ii)+".jpg")
+    width, height = img.size
+    left = 0
+    top = 0
+    right = width
+    bottom = height - 100
+    img2 = img.crop([left, top, right, bottom])
+    img2.load()
+    img2.save(str(ii)+"-crop.jpg")
+    ii += 1
