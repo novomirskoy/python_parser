b3fac60a3a396fffdabfbfad4606765d4eeb9fac
ding-ding <artyom.endin@gmail.com>
2014-04-07 18:03:26 +0400

    Почти работает

--- parser.py
+++ parser.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
+import json
 import time
+import locale
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+from bson.objectid import ObjectId
 from PIL import Image
 
+locale.setlocale(locale.LC_ALL, "")
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.adverts
@@ -50,67 +54,110 @@ def parse_advert_page(url_advert):
     for advert in adverts:
         print advert["advert_url"]
 
-
+params = {
+    u"Состояние": "state",
+    u"Двигатель": "engine_type",
+    u"КПП": "kpp",
+    u"Кузов": "body",
+    u"Цвет": "color",
+    u"Привод": "transmission",
+    u"Руль": "steering_wheel",
+    u"Владельцев по ПТС": "owners",
+    u"VIN-код": "vin",
+}
 # db.pages.remove()
-# db.amru.remove()
+# db.am.remove()
 # pages = get_count_pages(url)
 # print pages
 # parse_pages(pages)
 count_adverts = db.am.find().count()
 print "Количество объявлений: ", count_adverts
 
-for advert in db.am.find().limit(1):
+for advert in db.am.find():
     url = advert.get("advert_url")
     id = advert.get("_id")
-
+    # print url
     html_doc = urlopen(url).read()
     soup = BeautifulSoup(html_doc)
+    advert = {}
 
-    ul = soup.find("ul", class_="breadcrumbs")
-    brand = ul.li.a.span.string
+    brandTag = soup.find(attrs={"data-object": "brand"})
+    brand = brandTag.span.string
+    advert["brand"] = brand
 
-#
-#     images = []
-#     ul = soup.find_all("li", class_="b-rama-thumbs__item")
-#     if ul != []:
-#         for li in ul:
-#             image = li.a["data-original"]
-#             images.append(image)
-            #db.am_ru.update({"_id: id"}, {"advert_images": images})
-            # fileimage = urlopen(image).read()
-            # f = open(str(image[-36:]), "wb")
-            # print str(image[-36:])
-            # f.write(fileimage)
-            # f.close
+    modelTag = soup.find(attrs={"data-object": "model"})
+    model = modelTag.span.string
+    advert["model"] = model
+
+    yearTag = soup.find(attrs={"data-object": "year"})
+    year = yearTag.string
+    advert["year"] = year[1:]
+
+    images = []
+    images_ul = soup.find_all("li", class_="b-rama-thumbs__item")
+
+    if bool(images_ul) is not False:
+        for li in images_ul:
+            try:
+                image = li.a["data-original"]
+            except KeyError:
+                continue
+            else:
+                images.append(image)
+    advert["images"] = images
+
+    try:
+        custom_house_state = soup.find("span", class_="b-card-status__text").string
+    except AttributeError:
+        advert["custom_house_state"] = ""
+    else:
+        custom_house_state = custom_house_state.split(",")[0]
+        advert["custom_house_state"] = custom_house_state
+
+    params_table = {}
+    tables = soup.find_all("table", class_="b-card-info__table")
+    for table in tables:
+        for tr in table:
+            row = tr.find_all("td")
+            try:
+                key = row[0].get_text()
+                key = key[:-1]
+                value = row[1].get_text()
+            except IndexError:
+                span = row[0].find_all("span")
+                key = span[0].get_text()
+                key = key[:-1]
+                value = span[1].get_text()
+                params_table[key] = value
+            else:
+                params_table[key] = value
+
+    for key in params_table.keys():
+        try:
+            new_key = params.get(key)
+        except KeyError:
+            continue
+        else:
+            advert[new_key] = params_table[key]
+
+    engine = advert.get("engine_type")
+    engine = engine.split("/")
+    try:
+        advert["engine_type"] = engine[2].strip()
+    except IndexError:
+        advert["engine_power"] = ""
+    else:
+        advert["engine_volume"] = engine[0].strip()
+        advert["engine_type"] = engine[1].strip()
+
+    for key in advert.keys():
+        print "%s => %s" % (key, advert[key])
+
+    db.am.update({"_id": id}, {"$set": {"advert": json.dumps(advert)}})
 
 
-    #db.am_ru.insert({"advert_photos": images})
 
-# for advert in db.am_ru.find():
-#     print advert.get("advert_url")
-#     advert_url = str(advert.get("advert_url"))
-#     print advert_url
-#     html_doc = urlopen(advert_url).read()
-#     soup = BeautifulSoup(html_doc)
-#
-#     images = soup.find("li", class_="b-rama-thumbs__item")
-#     for image in images:
-#         image_soup = BeautifulSoup(image)
-#         src = image_soup.li
-#         print src
-#     #print type(images)
-#     #db.am_ru_adverts.insert({"advert_html": advert_html})
-#
-#     time.sleep(300)
 
-# html_doc = urlopen("http://nnov.am.ru/used/hyundai/ix35/avs-avtoregion-nn--9fa3edb9/#snp3").read()
-# soup = BeautifulSoup(html_doc)
-#
-# images = []
-# ul = soup.find_all("li", class_="b-rama-thumbs__item")
-# for li in ul:
-#     images.append(li.a["data-original"])
-#
 # ii = 0
 # for image in images:
 #     # fileimage = urlopen(image).read()
